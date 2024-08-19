4d3414cb69c35d54ec7c39e691290d1aea371a62
Dinesh1472 <dd2780277@gmail.com>
2024-08-19 19:08:42 +0530

    first commit

---
+++ Tuples.py
@@ -0,0 +1,8 @@
+try:
+    temp=('elegant')
+    print(type(temp))
+
+    temp =('elegant',)
+    print(temp)
+except Exception as e:
+    print(e)
---
+++ convert-tupleto-list.py
@@ -0,0 +1,12 @@
+try:
+    temp =(12,'sang','python',30.5)
+
+    temp1 = list(temp)
+    print(temp1)
+
+    temp1.append('elegant')
+
+    temp2 =tuple(temp1)
+    print(temp2)
+except Exception as e:
+    print(e)
